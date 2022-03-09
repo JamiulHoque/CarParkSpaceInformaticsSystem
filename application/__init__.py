12c0b54093cc4cedbaf9d46e368b7b8ed04e23c5
Ubuntu <ubuntu@ip-172-31-4-93.eu-west-2.compute.internal>
2022-03-09 01:57:45 +0000

    merging into main

--- application/__init__.py
+++ application/__init__.py
@@ -8,7 +8,7 @@ SECRET_KEY = os.urandom(32)
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://admin:Password1!@database.cop4kgvc8v0g.eu-west-2.rds.amazonaws.com/carpark
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://admin:Password1!@database.cop4kgvc8v0g.eu-west-2.rds.amazonaws.com/carpark"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = SECRET_KEY
 db = SQLAlchemy(app)
